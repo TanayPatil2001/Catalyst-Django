484ddea2282b9042241972c248c1d4eef5bd1a9c
TanayPatil <73037585+TanayPatil2001@users.noreply.github.com>
2024-07-02 13:13:45 +0530

    Update views.py

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,20 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout, login
 from django.contrib import messages
-from .forms import CustomUserCreationForm,LargeFileForm, FileDataForm, LargeFile
+from .forms import CustomUserCreationForm,LargeFileForm
 from django.conf import settings
-from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.models import User
-from .models import LargeFile, file_data
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from django.http import JsonResponse
 import pandas as pd
-import time
-from sqlalchemy import create_engine
-import csv
-import os
 from django.core.management.base import BaseCommand
 import uuid
 from django.views.decorators.csrf import csrf_exempt
@@ -67,8 +59,9 @@ def upload_file(request):
 
 uploads = {}
 uploads_lock = threading.Lock()
-df = None #  Global variable to store the DataFrame
+df = None 
 df_lock = threading.Lock()
+
 @csrf_exempt
 def upload_chunk(request):
     if request.method == 'POST':
@@ -90,15 +83,13 @@ def upload_chunk(request):
 
             uploads[upload_id]['chunks'][chunk_number] = chunk.read()
 
-            # Check if all chunks are uploaded
             if len(uploads[upload_id]['chunks']) == total_chunks:
-                # Reassemble the file
                 final_file_content = b''
                 for i in range(total_chunks):
                     final_file_content += uploads[upload_id]['chunks'][i]
 
-                # Convert the file content to a pandas DataFrame
-                global df  # Add this line to ensure df is recognized as a global variable
+                
+                global df 
                 with df_lock:
                     try:
                         from io import BytesIO
@@ -107,7 +98,6 @@ def upload_chunk(request):
                     except Exception as e:
                         print(f"Error reading CSV: {e}")
 
-                # Optionally, remove the chunk data to save memory
                 del uploads[upload_id]['chunks']
                 del request.session['upload_id']
 
@@ -133,17 +123,14 @@ def user_list(request):
 def count_records(request):
     global df
     if request.method == 'GET':
-        # Fetch parameters from request
         search_name = request.GET.get('name', '')
         search_domain = request.GET.get('domain', '')
         search_country = request.GET.get('country', '')
         search_year = request.GET.get('year_founded', '')
         search_industry = request.GET.get('industry', '')
-        # Check if df is None
         if df is None:
             print("DataFrame is None. Please upload data.")
-            return JsonResponse({'count': 0})  # Return count as 0 or handle differently
-        # Example filtering assuming df is your DataFrame
+            return JsonResponse({'count': 0})  
         filtered_data = df
         if search_country:
             search_year = int(search_year)
@@ -154,11 +141,9 @@ def count_records(request):
                 (df['year founded'] == search_year) &
                 (df['industry'] == search_industry)
             ]
-        # Calculate number of records
-        num = filtered_data.shape[0]  # Adjust as per your DataFrame structure
+
+        num = filtered_data.shape[0]  
         return JsonResponse({'count': num})
     elif request.method == 'POST':
-        # Handle POST method if necessary
         pass
-    # Handle other cases or errors
     return JsonResponse({'error': 'Method not allowed'}, status=405)
